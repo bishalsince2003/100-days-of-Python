81033230e455e1b2a34e3c347392e5b83cf9ee53
Bishal Kumar <133111722+bishalsince2003@users.noreply.github.com>
2026-06-14 02:16:09 +0530

    Completed Day 1 - Band Name Generator

---
+++ Day-01-Band-Name-Generator/main.py
@@ -0,0 +1,8 @@
+print("Welcome to the band name generator")
+
+city= input("Whats the name of your city where you grew up?\n")
+
+animal=input("Whats the name of your favourite animal?\n")
+
+print("Your band name could be: " + city + " " +  animal)
+
