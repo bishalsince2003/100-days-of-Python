95094e67e58d8101ad8197716fc43d4de9cd7959
Bishal Kumar <133111722+bishalsince2003@users.noreply.github.com>
2026-06-21 01:52:27 +0530

    Fix person assignment in game loop

--- Day-14-Higher-Lower-Game-Project/main.py
+++ Day-14-Higher-Lower-Game-Project/main.py
@@ -16,8 +16,8 @@ print(art.logo)
 
 
 game_over=False
+person = game_data.data[generate_new_num()]
 while game_over == False:
-    person = game_data.data[generate_new_num()]
     person1 = game_data.data[generate_new_num()]
     print(f"Compare A: {person['name']}, {person['description']}, {person['country']}, ")
 
@@ -40,6 +40,7 @@ while game_over == False:
     if user_value > computer_value:
        score+=1
        print(f"You're right! Current score: {score}")
+       person=person1
     elif computer_value > user_value:
         print(f"Sorry that's wrong. Final score: {score}")
         game_over=True
